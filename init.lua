7c7dbdd450f8e471c74c1bde8cf8c7df00d103e6
Pratik K <undopatika@gmail.com>
2026-09-15 21:33:52 +0530

    chore: synchronize environment changes

--- init.lua
+++ init.lua
@@ -6,4 +6,9 @@ vim.g.maplocalleader = "\\"
 -- Load the package manager configuration
 require("config.lazy")
 
+-- Indentation options (4 spaces)
+vim.opt.tabstop = 4        -- Number of spaces a <Tab> counts for
+vim.opt.softtabstop = 4    -- Number of spaces inserted when editing
+vim.opt.shiftwidth = 4     -- Size of an indent
+vim.opt.expandtab = true   -- Convert tabs to spaces
 
